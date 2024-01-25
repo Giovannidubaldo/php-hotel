da55573b43e0311c1eeba5b178d4abe819aaee6c
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-01-25 15:56:50 +0100

    Created header

--- partials/templates/header.php
+++ partials/templates/header.php
@@ -1,3 +1,10 @@
 <header>
-    
+    <div class="container">
+        <div class="row">
+            <div class="d-flex justify-content-between align-items-center">
+                <h1>Booking.com</h1>
+                <h3>Trova i migliori hotel adatti a te!</h3>
+            </div>
+        </div>
+    </div>
 </header>
